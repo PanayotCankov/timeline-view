a210264c9f9f502cf33cd7c55d82a569cdd12c07
sis0k0 <stanimira.vlaeva@gmail.com>
2017-10-24 14:23:04 +0300

    refactor: export toTrace and saveReport functions

--- timeline.ts
+++ timeline.ts
@@ -12,15 +12,8 @@ var timeline = [];
 
 var firstPoint = undefined;
 readlineInterface.on("line", line => {
-    var result = line.match(timeLog);
-    if (result) {
-        var trace = {
-            domain: result[2] && result[2].trim().replace(":", ""),
-            name: result[3].trim(),
-            from: parseFloat(result[4]),
-            to: parseFloat(result[5])
-        }
-        firstPoint = firstPoint || trace.from;
+    var trace = toTrace(line);
+    if (trace) {
         console.log(`Timeline: ${trace.name}  (${Math.floor(trace.from - firstPoint)} - ${Math.floor(trace.to - firstPoint)}) [${Math.floor(trace.to - trace.from)}]`);
         timeline.push(trace);
     } else {
@@ -28,23 +21,53 @@ readlineInterface.on("line", line => {
     }
 });
 
+export function toTrace(text) {
+    var result = text.match(timeLog);
+    if (!result) {
+        return;
+    }
+
+    var trace = {
+        domain: result[2] && result[2].trim().replace(":", ""),
+        name: result[3].trim(),
+        from: parseFloat(result[4]),
+        to: parseFloat(result[5])
+    }
+    firstPoint = firstPoint || trace.from;
+
+
+    return trace;
+}
+
 var once = false;
 function logOnExit() {
     if (once) return;
     once = true;
 
     console.log("Exiting... Print charts...");
+
+    saveReport(timeline, "times.html");
+    process.exit(0);
+}
+
+export function saveReport(timeline, destination) {
     var range = timeline.reduce((acc, line) => {
         return { from: Math.min(acc.from, line.from), to: Math.max(acc.to, line.to)}
     }, { from: Number.POSITIVE_INFINITY, to: Number.NEGATIVE_INFINITY });
     range.name = "ALL";
     timeline.push(range);
 
-    function dumpfile(file: string) {
-        return fs.readFileSync(path.join(__dirname, file)).toString();
-    }
+    var chart = getChart(timeline);
 
-    var file = `<!DOCTYPE html>
+    var absolutePath = path.resolve(destination);
+    console.log("\nTimeline report at: " + chalk.underline("file://" + absolutePath));
+
+    fs.writeFileSync(absolutePath, chart);
+
+}
+
+function getChart(timeline) {
+    return `<!DOCTYPE html>
 <html>
 <head>
 <title>Profiling</title>
@@ -59,12 +82,10 @@ ${dumpfile("index.js")}
   </script>
 </body>
 </html>`;
+}
 
-    var absolutePath = path.resolve("times.html");
-    fs.writeFileSync(absolutePath, file);
-    console.log();
-    console.log("Timeline report at: " + chalk.underline("file://" + absolutePath));
-    process.exit(0);
+function dumpfile(file: string) {
+    return fs.readFileSync(path.join(__dirname, file)).toString();
 }
 
 process.on('SIGINT', logOnExit);
